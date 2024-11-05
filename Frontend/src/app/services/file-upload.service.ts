489f11139b58481feb2f1d437467418f9c5e0031
Aziz Carducci <carducciaziz@gmail.com>
2024-11-05 02:35:13 +0100

    feat: rudimentäres Frontend für Fileupload feat: Verbindung zur API über Service feat: logik für Files schicken an backend

---
+++ Frontend/src/app/services/file-upload.service.ts
@@ -0,0 +1,31 @@
+// file-upload.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+  private apiUrl = 'http://localhost:8080/api/filehandler/upload'; // API-URL des Spring Boot-Backends
+
+  constructor(private http: HttpClient) {}
+
+  uploadFile(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.http.post(this.apiUrl, formData, {
+      headers: new HttpHeaders({
+        'Content-Type': 'multipart/form-data'})
+    }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('File upload error:', error);
+    return throwError(() => new Error('File upload failed, please try again.'));
+  }
+}
