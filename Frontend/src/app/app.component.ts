f7a362dea413871b4706b917cb773a4f7e8f3534
Aziz Carducci <carducciaziz@gmail.com>
2024-11-05 02:51:40 +0100

    feat: add file-handling to app.component

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FileUploadComponent} from './file-upload/file-upload.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FileUploadComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Filesharing-Frontend';
 }
