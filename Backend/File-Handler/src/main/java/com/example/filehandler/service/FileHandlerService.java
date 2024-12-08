4e78e4e74436a22b146e7bbc29bd0ae97fa5db76
Aziz Carducci <carducciaziz@gmail.com>
2024-12-08 01:06:15 +0100

    Revert "refactor: FileHandlerService differentiate between methods"
    This reverts commit bbd067b8bb74284a16c79803e0e17b2a294ee788.

--- Backend/File-Handler/src/main/java/com/example/filehandler/service/FileHandlerService.java
+++ Backend/File-Handler/src/main/java/com/example/filehandler/service/FileHandlerService.java
@@ -2,54 +2,51 @@ package com.example.filehandler.service;
 
 import com.example.filehandler.dto.FileHandlerFileRequest;
 import io.minio.*;
+import io.minio.errors.*;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FileHandlerService {
     private final MinioClientFactory minioClientFactory;
 
-    public void createBuckets(String bucketName)
-    {
-        try {
-            MinioClient minioClient = minioClientFactory.createMinioClient();
-            boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
-            if (!isExist) {
-                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
-            }
-            log.info("Bucket {} created successfully", bucketName);
-        } catch (Exception e) {
-            log.error("Error occurred while creating Bucket '{}':", bucketName, e);
+    public void fileUpload(FileHandlerFileRequest uploadFileRequest) throws IOException, MinioException, NoSuchAlgorithmException, InvalidKeyException {
+        MinioClient minioClient = minioClientFactory.createMinioClient();
+
+
+        String bucketName = "baumbucket";
+        String objectName = uploadFileRequest.file().getOriginalFilename();
+        String contentType = uploadFileRequest.file().getContentType();
+
+        // Check if the bucket exists
+        boolean isBucketExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+
+        // Create the bucket if it doesn't exist
+        if (!isBucketExist) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            System.out.println("Bucket created: " + bucketName);
+        }
+        else {
+            System.out.println("Bucket already exists: " + bucketName);
+        }
+
+        // Upload the file using InputStream
+        try (InputStream inputStream = uploadFileRequest.file().getInputStream()) {
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(objectName)
+                            .stream(inputStream, uploadFileRequest.file().getSize(), -1)
+                            .contentType(contentType)
+                            .build()
+            );
+            System.out.println("File uploaded successfully: " + objectName);
         }
     }
-
-    public void uploadFile(String bucketName, FileHandlerFileRequest fileHandlerFileRequest) {
-        try {
-            MinioClient minioClient = minioClientFactory.createMinioClient();
-
-            //Get MetaData from File
-            String objectName = fileHandlerFileRequest.file().getOriginalFilename();
-            String contentType = fileHandlerFileRequest.file().getContentType();
-
-            //Upload File
-            try (InputStream inputStream = fileHandlerFileRequest.file().getInputStream()) {
-                minioClient.putObject(PutObjectArgs.builder()
-                        .bucket(bucketName)
-                        .object(objectName)
-                        .stream(inputStream, fileHandlerFileRequest.file().getSize(), -1)
-                        .contentType(contentType)
-                        .build());
-            }
-            log.info("File '{}' uploaded successfully to bucket '{}'", objectName, bucketName);
-        }
-        catch (Exception e) {
-            log.error("Error occurred while uploading file '{}'", fileHandlerFileRequest.file().getOriginalFilename(), e);
-        }
-    }
-
 
 }
